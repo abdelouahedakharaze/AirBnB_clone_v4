a298dde6e04909f8ee79c7c3c288f51891dc9e50
abdelouahed akharaze <abdelouahedpro1@gmail.com>
2024-05-31 12:30:24 +0100

    task 2 finished

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,29 @@
+// Execute script when DOM is loaded
+$(document).ready(function() {
+    // Initialize an empty object to store checked Amenity IDs
+    let checkedAmenities = {};
+
+    // Listen for changes on any input checkbox
+    $(document).on('change', "input[type='checkbox']", function() {
+        // If the checkbox is checked, add its ID and name to the checkedAmenities object
+        if (this.checked) {
+            checkedAmenities[$(this).data('id')] = $(this).data('name');
+        } 
+        // If the checkbox is unchecked, remove its ID from the checkedAmenities object
+        else {
+            delete checkedAmenities[$(this).data('id')];
+        }
+
+        // Get an array of checked Amenity names
+        let amenityNames = Object.values(checkedAmenities);
+
+        // Update the displayed list of checked amenities
+        if (amenityNames.length > 0) {
+            // Display the list of checked amenities separated by commas
+            $('div.amenities > h4').text(amenityNames.join(', '));
+        } else {
+            // If no amenities are checked, display a placeholder
+            $('div.amenities > h4').html('&nbsp;');
+        }
+    });
+});
