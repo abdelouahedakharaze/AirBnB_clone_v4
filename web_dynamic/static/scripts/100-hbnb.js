36aeebae9785c4a99f3b2267ee7eddc75887c7c4
abdelouahed akharaze <abdelouahedpro1@gmail.com>
2024-06-01 09:51:22 +0100

    checkboxUpdate

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,31 +1,36 @@
-// Execute script when DOM is loaded
 $(document).ready(function() {
-    // Initialize an empty object to store checked Amenity IDs
+    // Initialize objects to store checked IDs
     let checkedAmenities = {};
+    let checkedStates = {};
+    let checkedCities = {};
 
     // Listen for changes on any input checkbox
     $(document).on('change', "input[type='checkbox']", function() {
-        // If the checkbox is checked, add its ID and name to the checkedAmenities object
-        if (this.checked) {
-            checkedAmenities[$(this).data('id')] = $(this).data('name');
-        } 
-        // If the checkbox is unchecked, remove its ID from the checkedAmenities object
-        else {
-            delete checkedAmenities[$(this).data('id')];
-        }
-
-        // Get an array of checked Amenity names
-        let amenityNames = Object.values(checkedAmenities);
-
-        // Update the displayed list of checked amenities
-        if (amenityNames.length > 0) {
-            // Display the list of checked amenities separated by commas
-            $('div.amenities > h4').text(amenityNames.join(', '));
+        // Determine if the checkbox is for amenities, states, or cities
+        if ($(this).closest('.amenities').length) {
+            updateCheckedItems(this, checkedAmenities, 'div.amenities > h4');
+        } else if ($(this).closest('.locations').find('h3').text() === 'States') {
+            updateCheckedItems(this, checkedStates, 'div.locations > h4');
         } else {
-            // If no amenities are checked, display a placeholder
-            $('div.amenities > h4').html('&nbsp;');
+            updateCheckedItems(this, checkedCities, 'div.locations > h4');
         }
     });
+
+    // Function to update checked items and display
+    function updateCheckedItems(checkbox, checkedItems, displaySelector) {
+        if (checkbox.checked) {
+            checkedItems[$(checkbox).data('id')] = $(checkbox).data('name');
+        } else {
+            delete checkedItems[$(checkbox).data('id')];
+        }
+
+        let itemNames = Object.values(checkedItems);
+        if (itemNames.length > 0) {
+            $(displaySelector).text(itemNames.join(', '));
+        } else {
+            $(displaySelector).html('&nbsp;');
+        }
+    }
     
 // Task 3:
     $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
